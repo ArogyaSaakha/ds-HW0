5b7e262e2e409ba14a500967f491bd6cafce098d
ArogyaSaakha <113126774+ArogyaSaakha@users.noreply.github.com>
2023-01-27 21:47:29 -0500

    Add files via upload

---
+++ Hw0.java
@@ -0,0 +1,19 @@
+package com.gradescope.hw0; 
+import java.util.ArrayList;
+public class Hw0 {
+    public static int addAll(ArrayList<Integer> numbers){
+        int sum = 0;
+        for (int i = 0; i< 50; i ++){
+           sum = sum + numbers.get(i);
+        }
+        return sum;
+    }
+    public static void main (String[] args){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i< 50; i ++){
+            numbers.add(i);
+        }
+        int sum = addAll(numbers);
+        System.out.print(" The total sum of numbers are " + sum);
+    }
+}
